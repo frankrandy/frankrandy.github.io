7b8a2f7661870cc6d1b6447544aff55c9473b514
frankrandy <90923812+frankrandy@users.noreply.github.com>
2021-09-20 10:43:38 -0600

    tbpp1is 0.2

--- thebigproject_part1_interactivescene/sketch.js
+++ thebigproject_part1_interactivescene/sketch.js
@@ -1,9 +1,12 @@
-// Project Title
-// Your Name
-// Date
+// fake clone of breakout/pong
+// Roland RFD
+// 9/20/2021
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Project Goal
+// Make a game where you control a pong racket to bounce a ball back and forth towards breaking blocks,
+// all while an AI enemy tries to break your blocks
+
+let x, y, speed;
 
 
 function setup() {
@@ -12,4 +15,11 @@ function setup() {
 
 function draw() {
   background(220);
+  drawPlayerRacket();
+}
+
+function drawPlayerRacket() {
+  let width = 20;
+  let height = 20;
+  rect(random(), random(), height, width);
 }
