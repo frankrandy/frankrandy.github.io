6d1b3efe3f5d6d0027bb3776adb15541fd909c1f
frankrandy <90923812+frankrandy@users.noreply.github.com>
2021-09-23 10:37:12 -0600

    bigproject 0.2
    added a whole lot of stuff like the tennisBall, very important

--- thebigproject_part1_interactivescene/sketch.js
+++ thebigproject_part1_interactivescene/sketch.js
@@ -5,9 +5,16 @@
 // Project Goal
 // Make a game where you control a pong racket to bounce a ball back and forth towards breaking blocks,
 // all while an AI enemy tries to break your blocks
+// 
+// Links ("its a good thing you got me to help you frank" -Frank's inner
+//conscious, 2021)
+//-keycode.info for key code values for javascript
+//-http://www.jeffreythompson.org/collision-detection/ hit detection
 
-let x, y, speed;
-
+let speed = 1;
+let x = 20;
+let y = 20;
+let hit = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,10 +23,49 @@ function setup() {
 function draw() {
   background(220);
   drawPlayerRacket();
+  tennisBall();
+
+  hit = collideRectRect(x, y, height, width);
+
+  print("colliding?", hit);
 }
 
 function drawPlayerRacket() {
+  //draws the Racket, aka the rect that bounces the tennisBall to the right
   let width = 20;
   let height = 20;
-  rect(random(), random(), height, width);
+  rect(x, y, height, width);
+  keyJockey();
+}
+
+function keyJockey() {
+  //I originally kept stealing this directly from the WASD demo until I
+  //remembered it to the point where I could type it from memory.
+
+  //key controls for exclusively drawPlayerRacket
+  if (keyIsDown(83)){
+    y -= speed;
+  }
+  if (keyIsDown(87)){
+    y += speed;
+  }
+  if (keyIsDown(68)){
+    x += speed;
+  }
+
+}
+
+function tennisBall(){
+  //its a tennis ball
+  tennisBall.height = 20;
+  tennisBall.width = 20;
+  tennisBall.x = 20;
+  tennisBall.y = 20;
+  rect(tennisBall.x *2, tennisBall.y *2, tennisBall.height, tennisBall.width );
+
+}
+
+function tennisBallbounce(){
+  //this function will make the tennisBall bounce randomly left
+  //to right depending on if it has been hit
 }
