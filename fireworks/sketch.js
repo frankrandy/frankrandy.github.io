9ffe296e49151fc9f1a691bb5a79c69850f75707
frankrandy <90923812+frankrandy@users.noreply.github.com>
2021-10-08 10:37:52 -0600

    added fireworks
    is pretty ;)

--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -1,9 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// firework demo
+
+let fireworks = [];
 
 
 function setup() {
@@ -11,5 +8,63 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("black");
+
+  for (let i=fireworks.length-1; i>=0; i--) {
+    if (fireworks[i].isDead()) {
+      //removeitsdeadass
+      fireworks.splice(i, 1);
+    }
+    else {
+      fireworks[i].update();
+      fireworks[i].display();
+
+    }
+
+  }
+}
+
+
+function mousePressed(){
+  for (let i=0; i<100; i++){
+    let someParticle = new Particle(mouseX, mouseY);
+    fireworks.push(someParticle);
+
+  }
+
+
+}
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 10;
+    this.dx = random(-5, 5);
+    this.dy = random(-5, 5);
+    this.alpha = 255; 
+    this.color = color(255, 0, 0, this.alpha);
+
+
+  }
+  display() {
+    this.color = color(255, 0, 0, this.alpha);
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+
+  }
+
+  update(){
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.alpha--;
+  }
+
+  isDead() {
+    return this.alpha < 0;
+
+  }
+
+
+  
 }
